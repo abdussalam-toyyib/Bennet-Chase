af9f601e9ca83d4aa887c8f0e6bace0def1c8f3d
Afolax <afolax08@gmail.com>
2025-09-01 16:37:15 +0100

    update to update to disappearing cta

--- src/components/shared/ButtonLink.tsx
+++ src/components/shared/ButtonLink.tsx
@@ -7,7 +7,7 @@ interface BottonLinkProps {
 
 export const BottonLink = ({href, text, classname = ''}: BottonLinkProps) => {
     return (
-        <a href={href} className={`px-6 py-3 border-none rounded-lg outline-none relative overflow-hidden border dark:bg-[#2BA0A6FF] hover:bg-[#53aeb3] cursor-pointer transform transition duration-300 ${classname}`}>
+        <a href={href} className={`px-6 py-3 border-none rounded-lg outline-none relative overflow-hidden border bg-[#2BA0A6FF] hover:bg-[#53aeb3] cursor-pointer transform transition duration-300 ${classname}`}>
             <span className="relative z-10 text-white"> {text} </span>
         </a>
     )
